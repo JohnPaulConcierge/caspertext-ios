1ef905bf00802243578ed5bf6e3085706ef7ff0d
ghifarhossni <41300087+ghifarhossni@users.noreply.github.com>
2019-04-17 16:21:00 +0200

    Revert "Add Casper Sample"

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,30 +7,12 @@
 //
 
 import UIKit
-import CasperText
 
 class ViewController: UIViewController {
 
-    @IBOutlet var fields: [CasperTextField]!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        for f in fields {
-            f.addTarget(self, action: #selector(ViewController.didEndOnExit(sender:)),
-                        for: .editingDidEndOnExit)
-            f.returnKeyType = .next
-        }
-        fields.last?.returnKeyType = .done
-    }
-
-    @IBAction func didEndOnExit(sender: CasperTextField) {
-        let index = fields.firstIndex(of: sender)!
-        if index == fields.count - 1 {
-            _ = fields[index].resignFirstResponder()
-        } else {
-            _ = fields[index + 1].becomeFirstResponder()
-        }
+        // Do any additional setup after loading the view.
     }
 
 
